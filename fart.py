cf4cf9ea65d7b7f55aae79b93e427ef84d84dc3f
Ethan James <ethanjamesauto@vt.edu>
2024-05-01 21:46:36 -0400

    Update fart.py

--- fart.py
+++ fart.py
@@ -32,6 +32,16 @@ with open("400_l_5drop.txt", 'r') as fp:
 
 #print(data)
 
+import numpy as np
+# calculate rmse between rela and sigmoid data
+re = np.array(two_data["real"])
+si_2 = np.array(two_data["sigmoid"])
+si_5 = np.array(five_data["sigmoid"])
+rmse_2 = np.sqrt(np.mean((re - si_2)**2))
+rmse_5 = np.sqrt(np.mean((re - si_5)**2))
+print("rmse between real and sigmoid with dropout 0.2: ", rmse_2)
+print("rmse between real and sigmoid with dropout 0.5: ", rmse_5)
+
 plt.plot(five_data["real"][:200], color = "orange", label = "Real Stock Price")
 plt.plot(five_data["sigmoid"][:200], color = "green", label = "dropout 0.5")
 plt.plot(two_data["sigmoid"][:200], color = "red", label = "dropout 0.2")
